87ec1e76154e5d638524d7abcbc6dd42782ab5be
Aarushi Rathore <165875037+AarushiRathore@users.noreply.github.com>
2025-08-16 16:45:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<script type="module">
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
